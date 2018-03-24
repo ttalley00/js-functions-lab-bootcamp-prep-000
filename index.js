2c09f90f0f539759a27b22df4cb8f6fffcdb3f97
Trip Talley <ttalley00@yahoo.com>
2018-03-24 15:43:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // write your code below!
+function happyHolidays(string) {return ("Happy holidays!")}
+function happyHolidaysTo(string) 
+{if (string === "happyHolidaysTo(name)") return ("Happy holidays, ${you}!")}
